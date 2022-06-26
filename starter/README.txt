603d8b218aaf696c83d8d26d2762640243c7e65f
Sally Wong <sywong10@yahoo.com>
2022-06-26 16:23:23 -0400

    june26

---
+++ starter/README.txt
@@ -0,0 +1,2 @@
+
+psql -U postgres example < /Users/sallywong/Documents/Sally/classes/udacity/fullstack/FSND/projects/capstone/starter/choir.sql
--- starter/app.py
+++ starter/app.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:postgres@localhost:5432/example'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:postgres@localhost:5432/capstone'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
@@ -16,32 +16,44 @@ class Singer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
     phone = db.Column(db.String(), nullable=False)
-    voicePart = db.Column(db.String(), nullable=False)
-    Days_Not_Available = db.Column(db.String(), nullable=True)
+    voice_part = db.Column(db.String(), nullable=False)
+    not_available = db.Column(db.String(), nullable=False)
+    enrollment = db.relationship('ChoirEnrollment', backref='singer', lazy=True)
 
-class Soloist(db.Model):
-    __tablename__ = 'soloist'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=True)
-    choir = db.Column(db.String(), nullable=True)
+
+# class Soloist(db.Model):
+#     __tablename__ = 'soloist'
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(), nullable=True)
+#     choir = db.Column(db.String(), nullable=True)
 
 
 class Choir(db.Model):
     __tablename__ = 'choir'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=True)
     practice_time = db.Column(db.String(), nullable=True)
+    enrollment = db.relationship('ChoirEnrollment', backref='choir', lazy=True)
+#
+#
+#
+
+class ChoirEnrollment(db.Model):
+    __tablename__ = 'enrollment'
+
+    id = db.Column(db.Integer, primary_key=True)
+    choir_id = db.Column(db.Integer, db.ForeignKey('choir.id'), nullable=False)
+    singer_id = db.Column(db.Integer, db.ForeignKey('singer.id'), nullable=False)
 
 
 
 
 
-try:
-    db.create_all()
-except Exception as e:
-    print(e)
-
-
+# try:
+#     db.create_all()
+# except Exception as e:
+#     print(e)
 
 
 
@@ -49,3 +61,6 @@ except Exception as e:
 @app.route('/')
 def index():
     return 'hello '
+
+# @app.route('/register_singer'):
+# def register_singer():
