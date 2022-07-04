a7abd5cbb5ce0aabe7fce808559e499c9716e02f
Sally Wong <sywong10@yahoo.com>
2022-07-04 13:34:31 -0400

    july 4th

--- starter/app.py
+++ starter/app.py
@@ -1,66 +1,108 @@
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:postgres@localhost:5432/capstone'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
-
-migrate = Migrate(app, db)
-
-
-class Singer(db.Model):
-    __tablename__ = 'singer'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
-    phone = db.Column(db.String(), nullable=False)
-    voice_part = db.Column(db.String(), nullable=False)
-    not_available = db.Column(db.String(), nullable=False)
-    enrollment = db.relationship('ChoirEnrollment', backref='singer', lazy=True)
-
-
-# class Soloist(db.Model):
-#     __tablename__ = 'soloist'
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(), nullable=True)
-#     choir = db.Column(db.String(), nullable=True)
-
-
-class Choir(db.Model):
-    __tablename__ = 'choir'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=True)
-    practice_time = db.Column(db.String(), nullable=True)
-    enrollment = db.relationship('ChoirEnrollment', backref='choir', lazy=True)
-#
-#
-#
-
-class ChoirEnrollment(db.Model):
-    __tablename__ = 'enrollment'
-
-    id = db.Column(db.Integer, primary_key=True)
-    choir_id = db.Column(db.Integer, db.ForeignKey('choir.id'), nullable=False)
-    singer_id = db.Column(db.Integer, db.ForeignKey('singer.id'), nullable=False)
+from flask_cors import CORS
+from models import setup_db, db, Singer, Choir, ChoirEnrollment
+# from models import setup_db, db
 
 
 
+def create_app(test_config=None):
+    app = Flask(__name__)
+    migrate = Migrate(app, db)
+
+    setup_db(app)
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
 
-# try:
-#     db.create_all()
-# except Exception as e:
-#     print(e)
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+        return response
+
+
+    @app.route('/')
+    def index():
+        return 'hello'
+
+
+    @app.route('/singers', methods=['GET'])
+    def get_singers():
+
+      singers = Singer.query.all()
+
+      return jsonify({
+            'success': True,
+            'singers': [s.long() for s in singers]
+        }), 200
+
+
+    @app.route('/singers', methods=['POST'])
+    def add_singers():
+        body = request.get_json()
+
+        new_name = body.get('name', None)
+        new_phone = body.get('phone', None)
+        new_voice_part = body.get('voice_part', None)
+        new_not_available = body.get('not_available', None)
+
+        try:
+            singer = Singer(name=new_name, phone=new_phone, voice_part=new_voice_part, not_available=new_not_available)
+            singer.insert()
+
+            return jsonify({
+              'success': True,
+              'singers': 'singer added'
+            }), 200
+
+        except Exception as e:
+            print(e)
+            abort(404)
+
+
+    # @app.route('/singers/<int:id>/voice_type', methods=['GET'])
+    # def get_singer_type(id):
+    #
+    #     try:
+    #         singers = Singer.query.filter(Singer.id==id).all()
+    #         # print(singer.voice_part)
+    #
+    #         for s in singers:
+    #             print(s.id)
+    #             print(s.voice_part)
+    #
+    #     except Exception as e:
+    #         print(e)
+    #         # abort(404)
+    #
+    #     return('from voice_type')
+
+
+    # @app.route('/singers/<int:id>/voice_type', method=['GET'])
+    # def get_singer_by_type():
 
 
 
 
-@app.route('/')
-def index():
-    return 'hello '
+    # @app.route('/singers/<int:id>/name', method=['GET'])
 
-# @app.route('/register_singer'):
-# def register_singer():
+
+
+
+    @app.route('/choirs', methods=['GET'])
+    def get_choirs():
+
+        all_choirs = Choir.query.all()
+
+        for c in all_choirs:
+            print(c.id)
+            print(c.name)
+
+        return('all choir names')
+
+
+    return app
+
+# @app.route('/add_singer'):
+# def add_singer():
--- starter/models.py
+++ starter/models.py
@@ -1,1 +1,77 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+from settings import DB_NAME, DB_USER, DB_PASSWORD
 
+
+database_path = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, 'localhost:5432', DB_NAME)
+db = SQLAlchemy()
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class Singer(db.Model):
+    __tablename__ = 'singer'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    phone = db.Column(db.String(), nullable=False)
+    voice_part = db.Column(db.String(), nullable=False)
+    not_available = db.Column(db.String(), nullable=False)
+    enrollment = db.relationship('ChoirEnrollment', backref='singer', lazy=True)
+
+    # def __init__(self, name, phone, voice_part, not_available):
+    #     self.name = name
+    #     self.phone = phone
+    #     self.voice_part = voice_part
+    #     self.not_available = not_available
+
+    def short(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'voice_part': self.voice_part
+        }
+
+
+    def long(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'phone': self.phone,
+            'voice_part': self.voice_part,
+            'not_avilable': self.not_available
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+    def __repr__(self):
+        return json.dumps(self.short())
+
+
+
+class Choir(db.Model):
+    __tablename__ = 'choir'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=True)
+    practice_time = db.Column(db.String(), nullable=True)
+    enrollment = db.relationship('ChoirEnrollment', backref='choir', lazy=True)
+
+
+
+class ChoirEnrollment(db.Model):
+    __tablename__ = 'enrollment'
+
+    id = db.Column(db.Integer, primary_key=True)
+    choir_id = db.Column(db.Integer, db.ForeignKey('choir.id'), nullable=False)
+    singer_id = db.Column(db.Integer, db.ForeignKey('singer.id'), nullable=False)
